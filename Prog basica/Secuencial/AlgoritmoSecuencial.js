8a5f95ba6798b1e75fbe60d0946bde74bd82f94d
MartinMT2020 <tinchomolina1981@gmail.com>
2021-04-19 21:47:29 -0300

    Ejercicios clase 1 y 2

---
+++ Prog basica/Secuencial/AlgoritmoSecuencial.js
@@ -0,0 +1,5 @@
+console.log("este");
+console.log("algoritmo");
+console.log("es");
+console.log("secuencial");
+console.log(3+6);
---
+++ Prog basica/Secuencial/algoritmoSecVariables.js
@@ -0,0 +1,5 @@
+let mens1 = "este ";
+let mens2 = "mensaje ";
+let mens3 = "es ";
+let mens4 = "secuencial";
+console.log(mens1,mens2,mens3,mens4);
