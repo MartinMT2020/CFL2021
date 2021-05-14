5895befaff31defd3f432f6dc8e88595d904c895
MartinMT2020 <tinchomolina1981@gmail.com>
2021-05-14 18:48:37 -0300

    variantes de invertir arreglo

--- Prog basica/Arreglos/clase 8/invertirArr.js
+++ Prog basica/Arreglos/clase 8/invertirArr.js
@@ -1,25 +1,45 @@
+"use strict";
 let readlineSync = require("readline-sync");
 let arrLength = readlineSync.questionInt("Ingrese la dimension del arreglo: ");
 let arrNumeros = new Array(arrLength);
 let arrNumeros2 = new Array(arrLength);
 
 //cargar arr con valores elegidos por el usuario
-for (i = 0; i < arrLength; i++) {
-  arrNumeros[i] = readlineSync.questionInt("Ingrese un numero para la pos " + i + ": ");
-  arrNumeros2[i] = Math.floor(Math.random()*10);
+for (let i = 0; i < arrLength; i++) {
+  arrNumeros[i] = readlineSync.questionInt(
+    "Ingrese un numero para la pos " + i + ": "
+  );
+  arrNumeros2[i] = Math.floor(Math.random() * 10);
 }
 
-//invertir arreglo haciendo swap
-function invertirArreglo(arr){
-    for (i = 0; i < arr.length / 2; i++) {
-      let j = arr.length - 1 - i;
-      let auxiliar = arr[i];
-      arr[i] = arr[j];
-      arr[j] = auxiliar;
-    }
+//invertir arreglo haciendo swap (variable "dinamica")
+function invertirArreglo(arr) {
+  let auxiliar;
+  for (let i = 0; i < arr.length / 2; i++) {
+    let j = arr.length - 1 - i;
+    auxiliar = arr[i];
+    arr[i] = arr[j];
+    arr[j] = auxiliar;
+  }
 }
 console.log(arrNumeros);
 console.log(arrNumeros2);
 invertirArreglo(arrNumeros2);
 console.log(arrNumeros2);
 
+//invertir arreglo haciendo swap (variante)
+function invertirVector(v, cantidad) {
+  let indiceIzq = 0;
+  let indiceDer = cantidad - 1;
+  let aux;
+  while (indiceIzq < indiceDer) {
+    aux = v[indiceIzq];
+    v[indiceIzq] = v[indiceDer];
+    v[indiceDer] = aux;
+    indiceIzq++;
+    indiceDer--;
+  }
+}
+console.log(arrNumeros2);
+invertirVector(arrNumeros2, arrLength);
+console.log(arrNumeros2);
