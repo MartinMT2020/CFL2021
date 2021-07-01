1bf8fc92e7a8177e6ace59b889f1156c501d76ad
MartinMT2020 <tinchomolina1981@gmail.com>
2021-07-01 18:34:14 -0300

    Metodos de Ordenamiento, Busqueda y Recursividad

---
+++ Prog basica/Recursivas/Practica ordenamiento y busqueda/OrdenarConArrAux.js
@@ -0,0 +1,12 @@
+/*
+• Desarrollar un programa que permita ordenar un arreglo “a” de
+tamaño “n” sin modificarlo, es decir, sin hacer los intercambios sobre la
+estructura “a”
+• Utilizar un arreglo auxiliar “aux” cargado con los índices del arreglo “a”
+(de 0 a
+• El ordenamiento tiene que hacerse mirando los valores de “a” pero
+haciendo los intercambios en “aux”
+• Crear un método que permita imprimir ordenado que reciba como
+parámetros “a”, “aux” y “n”
+*/
+
